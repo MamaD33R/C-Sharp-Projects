5a1995d67c78cba59d0531698b6c21a7ff989a45
Kaia F <kaialforrester@gmail.com>
2021-06-13 15:26:29 -0700

    DailyReport
    Daily Report Assignment - a questionnaire that also had to involve converting data types

---
+++ DailyReport/DailyReport/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("------------------"); // dashes for extra pizzazz
+
+             // beginning console questions
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+
+            Console.WriteLine("What course are you on?");
+            string yourCourse = Console.ReadLine();
+
+            Console.WriteLine("What page number?");     // converted string to int 
+            string yourPageNumber = Console.ReadLine();
+            int pageNumber = Convert.ToInt32(yourPageNumber);
+
+            Console.WriteLine("Do you need help with anything? Please answer: True or False");
+            string yourHelp = Console.ReadLine();
+            bool trueorfalse = bool.Parse(yourHelp); // converting string to boolean and returned converted answer
+
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string yourExperiences = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string yourFeedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            string yourHours = Console.ReadLine();
+            int studyHours = Convert.ToInt32(yourHours);
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+
+            Console.ReadLine(); // to keep console window open until closed
+        }
+    }
+}
