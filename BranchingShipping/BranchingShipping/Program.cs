2a95694a2c40b73a1422b77c1363805c21751b65
Kaia F <kaialforrester@gmail.com>
2021-06-19 20:05:57 -0700

    BranchingShipping
    Updated

--- BranchingShipping/BranchingShipping/Program.cs
+++ BranchingShipping/BranchingShipping/Program.cs
@@ -1,44 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace BranchingShipping
-{
+
+
+
     class Program
     {
-        static void Main(string[] args)
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+        Console.WriteLine("---vvv---");
+        Console.WriteLine("Please enter package weight: ");
+        int weight = Convert.ToInt32(Console.ReadLine());                   // if weight > 50, exit program, ELSE proceed for package width
+
+        if (weight > 50)
         {
-            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
-            Console.WriteLine("---vvv---");
-            Console.WriteLine("Please enter package weight: ");
-            int weight = Convert.ToInt32(Console.ReadLine());           // created weight variable for if else statement, will exit program if user enters a package weight more than 50
+            Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+        }
 
-            if (weight > 50)                                           // if weight > 50, exit program, ELSE proceed for package width
-            {
-                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
-            }
+        else
+        {
+            Console.WriteLine("Please enter package width: ");
+        }
 
-            else
-            {
-                Console.WriteLine("Please enter package width: ");
-            }
-            int width = Convert.ToInt32(Console.ReadLine());            // collecting width, height, length for dimensions
+        int width = Convert.ToInt32(Console.ReadLine());        // collecting width, height, length for dimensions
 
-            Console.WriteLine("Please enter package height: ");
-            int height = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please enter package height: ");
+        int height = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Please enter package length: ");
-            int length = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please enter package length: ");
+        int length = Convert.ToInt32(Console.ReadLine());
 
-            int dimensions = height * length * width * weight;          // gathering total dimensions of package
-            float quote = dimensions / 100;                             // taking measurements and dividing by 100 for quote for shipping price
+        int dimensions = height * 2 + length + width * 2;          // gathering total dimensions of package
+        float quote = dimensions * weight / 100;                           // taking measurements and dividing by 100 for quote for shipping price
 
+
+        if (dimensions > 50)
+        {
+            Console.WriteLine("Package too large to be shipped via Package Express. Have a good day.");
+        }
+        else
+        {
             Console.WriteLine("Your estimated total for shipping this package is: ${0}", quote);
             Console.WriteLine("Thank you!");
-
-            Console.ReadLine();
         }
+        Console.ReadLine();
+
     }
 }
+
