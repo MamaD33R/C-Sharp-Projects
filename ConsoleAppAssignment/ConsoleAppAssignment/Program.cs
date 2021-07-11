58a9122bd2efb8f799625ac57769236cc6e431c1
Kaia F <kaialforrester@gmail.com>
2021-07-11 15:45:45 -0700

    ConsoleApp
    Different iteration practice

---
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+    {
+        //string[] signIn = new string[5];
+        //    Console.WriteLine("Please add your name to sign-in sheet: ");
+
+        //    for (int i = 0; i < signIn.Length; i++)
+        //     {
+        //        signIn[i] = Console.In.ReadLine();
+        //     }
+
+        //    for (int i = 0; i < signIn.Length; i++)
+        //     {
+        //       Console.WriteLine(signIn[i] + " ");
+        //     }
+
+
+        // making infinite loop where value of a decrements "a--" but changed to "a++" to fix
+        //for (int a = 0; a < 50; a++)
+        //{
+        //    Console.WriteLine("number: {0}", a);
+        //}
+
+        //// a loop where the comparison is "<" operator 
+        //Console.WriteLine("Counting to 10");
+        //for (int i = 0; i < 11; i++)
+        //{
+        //    Console.WriteLine("{0}", i);
+        //}
+
+        // a loop where the comparison is "<=" operator 
+
+        //for (int i = 1; i <= 10; i++)
+        //{
+        //    Console.WriteLine("{0}", i);
+        //}
+
+        // A loop that returns index of item on list from user input
+
+        //List<string> nameList = new List<string>(); 
+
+        //nameList.Add("Gary");
+        //nameList.Add("Ethan");
+        //nameList.Add("Kaia");
+        //nameList.Add("Dayna");
+
+        //Console.WriteLine("Please search a name: ");
+        //string names = Console.ReadLine();
+
+        //if (nameList.Contains(names))
+        //{
+        //    for (int i = 0; i < nameList.Count; i++)
+        //    {
+        //        if (names == nameList[i])
+        //        {
+        //            Console.WriteLine(i);
+        //            break;
+        //        }
+        //    }
+
+        //} 
+        //else
+        //{
+        //    Console.WriteLine("Your entry is invalid.");
+
+        //}
+
+        //List<string> flavorList = new List<string>();
+
+        //flavorList.Add("Vanilla");
+        //flavorList.Add("Vanilla");
+        //flavorList.Add("Chocolate");
+        //flavorList.Add("Strawberry");
+        //flavorList.Add("Mint");
+
+        //Console.WriteLine("Please select a flavor: ");
+        //string choice = Console.ReadLine();
+
+        //if (flavorList.Contains(choice))
+        //{
+        //    foreach (string flavor in flavorList)
+        //    {
+        //        if (choice == flavor)
+        //        {
+        //            Console.WriteLine(flavorList.IndexOf(choice));
+        //        }
+        //    }
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Your entry is invalid.");
+        //}
+
+
+        //List<string> animalList = new List<string>();
+        //List<string> animalList2 = new List<string>(); // to hold previous items in main list while looping through each item
+
+        //animalList.Add("Bear");
+        //animalList.Add("Bear");
+        //animalList.Add("Chicken");
+        //animalList.Add("Moose");
+        //animalList.Add("Dog");
+
+        //foreach (string animal in animalList)
+        //{
+        //    foreach (string duplicate in animalList2)
+        //    {
+        //        if (duplicate == animal)
+        //        {
+        //            Console.WriteLine(duplicate + " has already been at this zoo.");
+        //        }
+        //    }
+        //    animalList2.Add(animal); 
+        //}
+
+        Console.ReadLine();
+        }
+    }
+
