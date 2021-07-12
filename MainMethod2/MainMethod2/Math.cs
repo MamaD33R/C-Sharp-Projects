a60c1ac935492996a0f207ce4d8220de17b25bf4
Kaia F <kaialforrester@gmail.com>
2021-07-12 15:25:38 -0700

    MainMethod2

---
+++ MainMethod2/MainMethod2/Math.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod2
+{
+    class Math
+    {
+        public static void Add(int x, int y = 0 )
+        {
+            Console.WriteLine("These numbers multiplied make: " + x * y);
+        }
+
+        public static void Add(int x)
+        {
+            Console.WriteLine("Your number is: " + x);
+        }
+
+    }
+}
---
+++ MainMethod2/MainMethod2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please enter a number: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You may enter another number to be multiplied with. Or not.");
+    
+            string num2 = Console.ReadLine();
+
+            if (num2 == "")
+            {
+                Math.Add(x);
+            }
+            else
+            {
+                int y = Convert.ToInt32(num2);
+                Math.Add(x, y);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
