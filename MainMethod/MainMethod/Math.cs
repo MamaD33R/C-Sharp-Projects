f3495c685a6cd82be4d23a7c62cf1b214f634efa
Kaia F <kaialforrester@gmail.com>
2021-07-12 13:18:46 -0700

    MainMethod
    Method Overloading

---
+++ MainMethod/MainMethod/Math.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Math
+    {
+        public static void Add(int x) // method overloading but passing in different types of parameters 
+        {
+            Console.WriteLine(x * 10);
+        }
+
+        public static void Add(decimal y)
+        {
+            Console.WriteLine(y * 42);
+        }
+
+        public static void Add(string i)
+        {
+            Console.WriteLine(i + 5);
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string i = "5";
+            int x = 20;
+            decimal y = 2.2m;
+
+            Math.Add(x);
+            Math.Add(y);
+            Math.Add(i);
+
+            Console.ReadLine();
+        }
+    }
+}
