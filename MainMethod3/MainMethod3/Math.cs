5d0cfd24e9644db8868e54a5eda393284b837144
Kaia F <kaialforrester@gmail.com>
2021-07-12 15:42:12 -0700

    MainMethod3

---
+++ MainMethod3/MainMethod3/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod3
+{
+    class Math
+    {
+        public static void Multi(int x, int y) // using x for math operation, and displaying the second integer
+        {
+            int result = x * 18;
+            Console.WriteLine(result);
+            Console.WriteLine(y); 
+
+        }
+    }
+}
---
+++ MainMethod3/MainMethod3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 16;
+            int y = 30;
+
+            Console.WriteLine("Passing in the numbers: 10 & 20.");
+            Math.Multi(10, 20);
+            Console.WriteLine("Calling parameters by name: ");
+            Math.Multi(x, y);
+
+            Console.ReadLine();
+        }
+    }
+}
